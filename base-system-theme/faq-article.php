332cb8534e3fd3ee756b014b90a69019be25f0e3
odenge <odenge@gmail.com>
2016-11-14 14:31:04 +0900

    よくあるご質問PHPファイル修正完了
    テンプレートに関する作成・修正が完了した

---
+++ base-system-theme/faq-article.php
@@ -0,0 +1,5 @@
+<li><div class="title"><span class="q">Q</span><?php the_title(); ?></div>
+    <div class="info clearfix"><span class="a">A</span>
+		<div class="info-content"><?php the_content(); ?></div>
+	</div>
+</li>
